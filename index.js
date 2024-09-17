84d30180ca156f3260c3b57e970a9cff04322851
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-09-17 11:21:37 +0900

    あああ

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-const osc = require("node-osc"); // node-oscをrequire
+const osc = require("node-osc"); // OSCモジュール node-oscをrequire
 console.log("loaded: node-osc");
 const models = require("./models.json"); // テラー情報などをrequire
 console.log("loaded: models")
+const chokidar = require("chokidar"); // ファイル監視 chokidarをrequire
+const path = require("path"); // パス取得 pathをrequire
+console.log("loaded: chokidar, path")
 const server = new osc.Server(9000, "127.0.0.1"); // OSCサーバーを初期化
 console.log("server is up, listening on port 9000");
 
 // 様々
 
 var roundType = 0; // ラウンドの種類
-var terror1   = 0; // 1体目のテラー
-var terror2   = 0; // 2体目のテラー
-var terror3   = 0; // 3体目のテラー
+var terror    = 0; // テラー名
 var optedin   = false; // ゲームに参加しているかどうか
 var saboteur  = false; // サボタージュラウンドにて憑依状態かどうか
 
 // コンソール表示関数
 
-function showData(roundType, terror1, terror2, terror3, optedin, saboteur) {
+function showData(roundType, terror, optedin, saboteur) {
     console.log(`
         received!
         現在のラウンド: ${roundType}
-        1体目のテラー: ${terror1}
-        2体目のテラー: ${terror2}
-        3体目のテラー: ${terror3}
+        テラー: ${terror}
         参加状態: ${optedin}
         憑依状態: ${saboteur}
     `)
 }
 
+// ログファイルを監視
+// テラー名を取得する
+
+// ログファイルまでのパスを取得
+home_dir = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+log_dir = path.join(logdir, "AppData/LocalLow/VRChat/VRChat");
+
 // ToNSaveManagerからラウンド情報などを受け取る
 // https://github.com/ChrisFeline/ToNSaveManager?tab=readme-ov-file#osc-documentation
-server.on("/avatar/parameters/ToN_Terror1", (osc_terror1) => {
-    terror1 = osc_terror1;
-})
-
-server.on("/avatar/parameters/ToN_Terror2", (osc_terror2) => {
-    terror2 = osc_terror2;
-})
-
-server.on("/avatar/parameters/ToN_Terror3", (osc_terror3) => {
-    terror3 = osc_terror3;
-})
 
 server.on("/avatar/parameters/ToN_OptedIn", (osc_optedin) => {
     optedin = osc_optedin;
@@ -53,5 +48,5 @@ server.on("/avatar/parameters/ToN_Saboteur", (osc_saboteur) => {
 server.on("/avatar/parameters/ToN_RoundType", (osc_roundType) => {
     osc_roundType = osc_roundType.toString();
     roundType = models["roundType"][osc_roundType.substring(osc_roundType.indexOf(",") + 1)];
-    showData(roundType, terror1, terror2, terror3, optedin, saboteur);
+    showData(roundType, terror, optedin, saboteur);
 })
