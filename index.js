4c745606eef559351bd45c4a2c3c3aa9e43d384b
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-09-17 12:10:07 +0900

    最新のログファイルを取得できるまでになった

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const osc = require("node-osc"); // OSCモジュール node-oscをrequire
+const osc = require("node-osc"); // OSCモジュール
 console.log("loaded: node-osc");
-const models = require("./models.json"); // テラー情報などをrequire
+const models = require("./models.json"); // テラー情報
 console.log("loaded: models")
-const chokidar = require("chokidar"); // ファイル監視 chokidarをrequire
-const path = require("path"); // パス取得 pathをrequire
-console.log("loaded: chokidar, path")
+const chokidar = require("chokidar"); // ファイル監視
+console.log("loaded: chokidar")
+const path = require("path"); // パス取得
+console.log("loaded: path")
+const fs = require("fs"); // ファイル操作
+console.log("loaded: fs")
 const server = new osc.Server(9000, "127.0.0.1"); // OSCサーバーを初期化
 console.log("server is up, listening on port 9000");
 
 // 様々
 
 var roundType = 0; // ラウンドの種類
-var terror    = 0; // テラー名
+var terror    = null; // テラー名
 var optedin   = false; // ゲームに参加しているかどうか
 var saboteur  = false; // サボタージュラウンドにて憑依状態かどうか
 
@@ -32,7 +35,24 @@ function showData(roundType, terror, optedin, saboteur) {
 
 // ログファイルまでのパスを取得
 home_dir = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
-log_dir = path.join(logdir, "AppData/LocalLow/VRChat/VRChat");
+log_dir = path.join(home_dir, "AppData/LocalLow/VRChat/VRChat/");
+
+// 最新のファイルを取得
+// VRChatディレクトリ直下のファイル ディレクトリを取得してソート
+const logdir_list = fs.readdirSync(log_dir).map(filename => {
+    return {
+        filename: filename,
+        mtime: fs.statSync(log_dir + filename).mtime
+    }
+})
+logdir_list.sort((a,b) => a.mtime - b.mtime );
+// output_log_*.txtでソート
+const filter_logfile = logdir_list.filter(logs => logs.filename.startsWith("output_log_") && logs.filename.endsWith(".txt"));
+// 最新のmtimeを持つオブジェクトのfilenameを抽出
+const latest_logfile = filter_logfile.reduce((a, b) => {
+    return new Date(a.mtime > b.mtime) ? b : a;
+})["filename"] // この書き方ええんか？
+console.log(latest_logfile)
 
 // ToNSaveManagerからラウンド情報などを受け取る
 // https://github.com/ChrisFeline/ToNSaveManager?tab=readme-ov-file#osc-documentation
