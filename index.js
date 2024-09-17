52770187f4f6e0bf0375905ff5d8b4447b90c5f6
Assault1892 <vvvvvgaga12356@yahoo.co.jp>
2024-09-17 10:31:28 +0900

    あ

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ console.log("starting...");
 const server = new osc.Server(9000, "127.0.0.1"); // OSCサーバーを初期化
 console.log("server is up, listening on port 9000");
 
+// 様々
+
+var roundType = 0; // ラウンドの種類
+var terror1   = 0; // 1体目のテラー
+var terror2   = 0; // 2体目のテラー
+var terror3   = 0; // 3体目のテラー
+var optedin   = false; // ゲームに参加しているかどうか
+var saboteur  = false; // サボタージュラウンドにて憑依状態かどうか
+
 // ToNSaveManagerからラウンド情報などを受け取る
 // https://github.com/ChrisFeline/ToNSaveManager?tab=readme-ov-file#osc-documentation
-server.on("/avatar/parameters/ToN_RoundType", (roundType) => {
-    console.log("received, round type: " + roundType);
+server.on("/avatar/parameters/ToN_RoundType", (osc_roundType) => {
+    roundType = osc_roundType;
+    console.log(`
+        received!
+        現在のラウンド: ${roundType}
+        1体目のテラー: ${terror1}
+        2体目のテラー: ${terror2}
+        3体目のテラー: ${terror3}
+        参加状態: ${optedin}
+        憑依状態: ${saboteur}
+    `)
 })
